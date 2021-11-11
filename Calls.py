84062f26c676b1914e482958c0c288521f2b5156
Segev955 <segev95@gmail.com>
2021-11-11 19:12:32 +0200

    fix "Calls" and start main

--- Calls.py
+++ Calls.py
@@ -1,17 +1,21 @@
 import csv
 
+
 class calls:
-    def __init__(self, elevator_call="", time=0, source=0, destination=0, status=0, elevator=0) -> None:
-        self.elevator_call = elevator_call
-        self.time = time
-        self.source = source
-        self.destination = destination
-        self.status = status
-        self.elevator = elevator
+    def __init__(self, callID=0) -> None:
+        row = []
+        f = open("Calls_a.csv")
+        csv_reader = csv.reader(f)
+        for line in csv_reader:
+            row.append(line)
+        self.callID = callID
+        self.elevator_call = row[callID][0]
+        self.time = row[callID][1]
+        self.source = row[callID][2]
+        self.destination = row[callID][3]
+        self.status = row[callID][4]
+        self.elevator = row[callID][5]
+        self.length = len(csv_reader)
 
     def __str__(self) -> str:
         return f"Elevator_call: {self.elevator_call} time: {self.time} Source: {self.source} Destination: {self.destination} Status: {self.status} Elevator: {self.elevator}"
-
-    # def __repr__(self) -> str:
-    #     return f"Elevator_call: {self.elevator_call} time: {self.time} Source: {self.source} Destination: {self.destination} Status: {self.status} Elevator: {self.elevator}"
-
--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 import json
 import csv
-from Ex1.Elevator import Elevator
+
+import pandas
+
+from Elevator import Elevator
+from Building import Building
+from Calls import calls
+
+
+def up_down(id: int):
+    call = calls(id)
+    x = call.destination - call.source
+    if x < 0:
+        return -1  # DOWN
+    return 1  # UP
+
+
+def up_or_down(call: calls):
+    up = 0
+    down = 0
+    for i in call:
+        if up_down(call.callID) == 1:
+            up += 1
+        if up_down(call.callID) == -1:
+            down += 1
+    if up > down:
+        return 1
+    if down < up:
+        return -1
+    return 0
+
 
 if __name__ == '__main__':
+    numOfEle = len(Building['_elevators'])
+    elevators = [numOfEle]
+    for j in calls:
+        c = calls(j)
+        if numOfEle == 1:
+            for i in numOfEle:
+                if up_or_down(calls) == 1:
 
