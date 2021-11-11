53939d0c114ed6fce617fd709219386fe92ddd3b
dani4655 <dani4655@gmail.com>
2021-11-11 18:10:16 +0200

    some changes and progress

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import json
 import csv
+import Calls
+
 from Ex1.Elevator import Elevator
 
 if __name__ == '__main__':
+    pass
+"""
+this function calculate the time it will take to the elevator to go from source floor to dest not including any stops in the way
+"""
+def time(source: int, dest: int, eleveator: Elevator):
+    return (abs(dest - source)) / eleveator._speed + eleveator._openTime + eleveator._closeTime + eleveator._startTime + eleveator._stopTime
 
+
+def timeAll(calls:[]):
+    for i in calls:
+        calls[i]
